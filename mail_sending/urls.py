fef7c2e86215a52783d9c2296763ab9cbc30e8cb
svro <ichistrawberry2012@gmail.com>
2023-08-24 18:38:26 +0300

    workspace_1_3

---
+++ mail_sending/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from mail_sending.views import index
+from .apps import MailSendingConfig
+
+# app_name = MailSendingConfig.name
+
+urlpatterns = [
+    path('', index)
+]
