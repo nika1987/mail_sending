3dac88b29c58b3768d813f1696abbde301448abd
svro <ichistrawberry2012@gmail.com>
2023-08-26 21:45:35 +0300

    client model and superuser

--- client/models.py
+++ client/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Client
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    '''Клиент сервиса'''
+    email = models.EmailField(unique=True, verbose_name='email')
+    name = models.CharField(**NULLABLE, max_length=150, verbose_name='ФИО')
+    comment = models.TextField(**NULLABLE, verbose_name='Комментарий')
+
+    def __str__(self):
+        return f'{self.email} {self.name}'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
