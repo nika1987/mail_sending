0c2e5b2a99c53fcc2a76889339bba73776708d96
svro <ichistrawberry2012@gmail.com>
2023-08-28 23:42:48 +0300

    working with static

--- mail_sending/views.py
+++ mail_sending/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'mail_sending/main.html')
+    return render(request, 'mail_sending/index.html')
