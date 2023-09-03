4b2f305bcf8424ae1cf35a1e4811cacab9e9db07
svro <ichistrawberry2012@gmail.com>
2023-09-03 17:16:39 +0300

    client perms

--- client/views.py
+++ client/views.py
@@ -4,13 +4,19 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy, reverse
 from client.forms import ClientForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 
-class ClientCreateView(CreateView):
+
+class ClientCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     '''CREATE - создается карточка клиента'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
+    '''PermissionRequiredMixin - доступ к изменению контента, permission_required = приложение.команда_модель'''
 
     model = Client
     form_class = ClientForm
     success_url = reverse_lazy('client:client_index')
+    permission_required = 'client.add_client'
 
     def form_valid(self, form):
         if form.is_valid():
@@ -20,28 +26,31 @@ class ClientCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ClientListView(ListView):
+class ClientListView(LoginRequiredMixin, ListView):
     '''READ - чтение списка клиентов'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
 
     model = Client
     template_name = 'client/client_index.html'
 
 
-
-class ClientDetailView(DetailView):
+class ClientDetailView(LoginRequiredMixin, DetailView):
     '''READ - чтение одной записи клиента'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
 
     model = Client
     success_url = reverse_lazy('client:client_detail')
 
 
-
-class ClientUpdateView(UpdateView):
+class ClientUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     '''UPDATE - обновление записи клиента'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
+    '''PermissionRequiredMixin - доступ к изменению контента, permission_required = приложение.команда_модель'''
 
     model = Client
     form_class = ClientForm
     success_url = reverse_lazy('client:client_index')
+    permission_required = 'client.change_client'
 
     def form_valid(self, form):
         if form.is_valid():
@@ -54,8 +63,11 @@ class ClientUpdateView(UpdateView):
         return reverse('client:client_detail', args=[self.kwargs.get('pk')])
 
 
-class ClientDeleteView(DeleteView):
+class ClientDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     '''DELETE - удаление записи клиента'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
+    '''PermissionRequiredMixin - доступ к изменению контента, permission_required = приложение.команда_модель'''
 
     model = Client
     success_url = reverse_lazy('client:client_index')
+    permission_required = 'client.delete_client'
