54ebfaeeb036ddb16aaa8c8d0766a53f61413f54
svro <ichistrawberry2012@gmail.com>
2023-09-03 16:14:47 +0300

    blog perms

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,19 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy, reverse
 from blog.forms import BlogForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 
-class BlogCreateView(CreateView):
+
+class BlogCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     '''CREATE - создается запись блога'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
+    '''PermissionRequiredMixin - доступ к изменению контента, permission_required = приложение.команда_модель'''
 
     model = Blog
     form_class = BlogForm
     success_url = reverse_lazy('blog:blog_index')
+    permission_required = 'blog.add_blog'
 
     def form_valid(self, form):
         if form.is_valid():
@@ -45,12 +51,15 @@ class BlogDetailView(DetailView):
         return self.object
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     '''UPDATE - обновление записи блога'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
+    '''PermissionRequiredMixin - доступ к изменению контента, permission_required = приложение.команда_модель'''
 
     model = Blog
     form_class = BlogForm
     success_url = reverse_lazy('blog:blog_index')
+    permission_required = 'blog.change_blog'
 
     def form_valid(self, form):
         if form.is_valid():
@@ -63,8 +72,11 @@ class BlogUpdateView(UpdateView):
         return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     '''DELETE - удаление записи блога'''
+    '''LoginRequiredMixin - скрывает контент от неавторизованных пользователей'''
+    '''PermissionRequiredMixin - доступ к изменению контента, permission_required = приложение.команда_модель'''
 
     model = Blog
     success_url = reverse_lazy('blog:blog_index')
+    permission_required = 'blog.delete_blog'
